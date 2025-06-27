e5ffffeb273bf13187320bee812748dde7603d41
RohanBhosale1011 <rohanbhosale1011@gmail.com>
2025-06-27 19:53:17 +0530

    initial  Commit.

---
+++ Ifelse/src/demo.java
@@ -0,0 +1,17 @@
+
+public class demo {
+	
+public static void main(String[] args) {
+	float  r= 3.0215f;
+	float area= 3.1416f*r*r;
+	if (r<=100)
+	{
+		System.out.println( "circle is big  and their area is "+area);
+		
+	}
+	else
+	{
+		System.out.println( " area is circle is "+area+" it is a small circle");
+	}
+}
+}
